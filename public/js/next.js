91eb1a28beb1df211c36a1f32e4de44f337711f4
Rashad Clement <yosh3655@gmail.com>
2022-09-30 03:13:24 -0700

    Set up app for Heroku

--- public/js/next.js
+++ public/js/next.js
@@ -12,10 +12,10 @@ weatherForm.addEventListener("submit", (event) => {
 
     const location = searchElement.value
 
-    messageOne.textContent = "Loading..."
+    messageOne.textContent = "Hold on, give me a second..."
     messageTwo.textContent = ""
 
-    fetch("http://localhost:3000/weather?address=" + location).then((response) => {
+    fetch("/weather?address=" + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
                 messageOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require("./utils/forecast")
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const address = process.argv[2]
 
 // Define path
